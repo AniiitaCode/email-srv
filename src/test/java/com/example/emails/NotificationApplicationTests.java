a81efcbfaec4d2f8136977aee5a872c9c6b8dfa5
AniiitaCode <anita_nen4eva@abv.bg>
2025-03-24 16:24:30 +0200

    Added annotation

--- src/test/java/com/example/emails/NotificationApplicationTests.java
+++ src/test/java/com/example/emails/NotificationApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.emails;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NotificationApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
